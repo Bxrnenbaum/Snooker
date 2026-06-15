38f7c576a59debb447ca6d63a857d97489c4808f
Bxrnenbaum <tobidercoolste@gmx.at>
2026-06-15 12:23:01 +0200

    fix: building issues

--- Snooker/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.snooker {
-        requires javafx.controls;
-        requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens com.example.snooker to javafx.graphics, javafx.fxml;
-        exports com.example.snooker;
-}
